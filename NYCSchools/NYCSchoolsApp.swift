bc2c478645ab1dfa42922d3009d1b013e8e41d6c
Chris Sanders <astericky@gmail.com>
2020-12-16 11:37:43 -0500

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Chris Sanders on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
