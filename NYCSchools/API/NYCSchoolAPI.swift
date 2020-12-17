3719111ca384103fe3fe1cabdd27e7cc4dbcce38
Chris Sanders <astericky@gmail.com>
2020-12-17 10:16:01 -0500

    add documentation

--- NYCSchools/API/NYCSchoolAPI.swift
+++ NYCSchools/API/NYCSchoolAPI.swift
@@ -22,9 +22,13 @@ class NYCSchoolsFetcher {
 }
 
 extension NYCSchoolsFetcher: NYCSchoolsFetchable {
+    // URLs to get NYC High School data
     static let highSchoolListURL = URL(string: "https://data.cityofnewyork.us/resource/s3k6-pzi2.json")
     static let highSchoolSATScoreURL = URL(string: "https://data.cityofnewyork.us/resource/f9bf-2cp4.json")
     
+    
+    /// Get general information for NYC High Schools
+    /// - Returns: AnyPublisher<[HighSchool], HighSchoolError>; returns a list of HS or an error
     func getHighSchools() -> AnyPublisher<[HighSchool], HighSchoolError> {
         guard let url = NYCSchoolsFetcher.highSchoolListURL else {
             let error = HighSchoolError.network(description: "Couldn't create url")
@@ -40,6 +44,9 @@ extension NYCSchoolsFetcher: NYCSchoolsFetchable {
             .eraseToAnyPublisher()
     }
     
+    
+    /// Get SAT data for NYC High Schools
+    /// - Returns: AnyPublisher<[HighSchoolSATScore], HighSchoolError>; returns a list of HS SAT Scores or an error
     func getHighSchoolSATScores() -> AnyPublisher<[HighSchoolSATScore], HighSchoolError> {
         guard let url = NYCSchoolsFetcher.highSchoolSATScoreURL else {
             let error = HighSchoolError.network(description: "Couldn't create url")
